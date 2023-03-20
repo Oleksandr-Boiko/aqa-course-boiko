572be35d522e6f71595b251584c582095c783ab3
Oleksandr Boiko <boiko.oleksandr@pdffiller.team>
2023-03-20 15:08:09 +0100

    add second comment

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         //First comment
+        //Second comment with some changes
     }
 }
