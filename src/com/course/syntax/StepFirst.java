19911d9fbd7b83255452dea29118d3c9a5f80762
Oleksandr Boiko <boiko.oleksandr@pdffiller.team>
2023-04-03 13:38:28 +0200

    Task 1

---
+++ src/com/course/syntax/StepFirst.java
@@ -0,0 +1,15 @@
+package com.course.syntax;
+
+public class StepFirst {
+    public static void main(String[] args) {
+        byte byteVar = 120;
+        short shortVar = 32111;
+        int intVar = 124124122;
+        long longVar = 1243243534;
+        float floatVar = 14.3f;
+        double doubleVar = 22.453;
+        char charVar = 'c';
+        boolean booleanVar = true;
+        System.out.println(byteVar + shortVar + intVar + longVar + floatVar + doubleVar + charVar + " " + booleanVar);
+    }
+}
