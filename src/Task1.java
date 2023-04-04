61cd7eb1b0cdb7e3a28d81a97a19588e5d647dfa
Oleksandr Boiko <boiko.oleksandr@pdffiller.team>
2023-04-04 19:45:15 +0200

    Task 1 - improvements

--- src/Task1.java
+++ src/Task1.java
@@ -5,7 +5,7 @@ public class Task1 {
         int[] array = {10, 20, 30, 40, 50, 60};
         int sum = 0;
         for (int i = 0; i < array.length; i++){
-            sum = sum + array[i];
+            sum+= array[i];
         }
         int average = sum / array.length;
         System.out.println(average);
