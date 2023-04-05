0139aa2b1f80621a6e8a3d3741ff102fac358b6a
Oleksandr Boiko <boiko.oleksandr@pdffiller.team>
2023-04-05 20:58:10 +0200

    Task 3

---
+++ src/Task3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your message: ");
+        String enteredMessage = scanner.nextLine();
+        int counter = 0;
+        for (int i = 0; i < enteredMessage.length(); i++){
+            if (enteredMessage.charAt(i) !=' ') {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+    }
+}
